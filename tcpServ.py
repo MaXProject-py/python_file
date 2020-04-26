e4f539dd800115688fa8768a3060401316c7be21
MaXProject-py <pytyler@mail.ru>
2020-04-26 15:19:04 +0400

    Добавил финкциональности серверу. Инфорация об ОС-[os], содержимое каталога [ls], текущий рабочий каталог [pwd]

--- tcpServ.py
+++ tcpServ.py
@@ -1,5 +1,6 @@
 from socket import *
 from time import ctime
+import os
 
 
 tcpServ = socket(AF_INET, SOCK_STREAM)  # создание обьекта сокета протакола TCP
@@ -14,10 +15,35 @@ while True:
     while True:
         data = tcpCli.recv(1024)  # размер буфера 1 Кбайт
         udata = data.decode('utf-8')  # декодирование последовательности байтов(bytes) в строку(str)
+
+
         if not data:
             print('Соединение разорвано!\n')
             break
+
+        print(udata)
         print('Отправка пакета на ', addr)
-        # кодирование строки и ее отправка с префиксом времени
-        tcpCli.send(b'[%s] %s ' % (bytes(ctime(), 'utf-8'), udata.encode('utf-8')))
+
+        if udata == "os":
+            while True:
+                tcpCli.send(b'[%s] %s ' % (bytes(os.name, 'utf-8'), udata.encode('utf-8')))
+                break
+
+        elif udata == 'ls':     # выводит содержимое каталога
+            while True:
+                ls_dir = os.listdir(path=".")
+                ls = ' '.join(ls_dir)        # Соединение списка в строку
+                tcpCli.send(b'%s' % (bytes(ls, 'utf-8')))
+                break
+
+        elif udata == 'pwd':
+            while True:
+                pwd = os.path.abspath(path='.')
+                tcpCli.send(b'%s' % (bytes(pwd, 'utf-8')))
+                break
+
+        else:
+            # кодирование строки и ее отправка с префиксом времени
+            tcpCli.send(b'[%s] %s ' % (bytes(ctime(), 'utf-8'), udata.encode('utf-8')))
+
     tcpCli.close()
